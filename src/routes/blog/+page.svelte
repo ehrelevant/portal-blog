3b4d3b5d909a1dc0e6c1adfb2f3a086e48f994d4
Ehren Castillo <ehrencastillo@gmail.com>
2024-09-05 02:23:04 +0800

    feat: add blogs page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,13 @@
-<div></div>
+<script>
+	import sample from '$lib/images/sample.png';
+	import BlogCard from './BlogCard.svelte';
+</script>
+
+<section class="grow h-full flex flex-col sm:flex-row justify-center gap-12 sm:gap-4 px-6">
+	<div class="relative hidden lg:flex justify-center min-h-full w-16 shrink-0">
+		<div class="min-h-full border-l-4 border-black" />
+	</div>
+	<div class="my-8 grid grid-flow-row auto-rows-min grid-cols-1 md:grid-cols-2 gap-4">
+		<BlogCard href="/blog/1" src={sample} title="Project Plan" date="09/05/2024" />
+	</div>
+</section>
---
+++ src/routes/blog/BlogCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+
+	export let href: string;
+	export let src: string;
+	export let title: string;
+	export let date: string;
+</script>
+
+<a
+	href="{base}{href}"
+	class="bg-white w-full h-fit rounded-xl p-2 md:p-4 flex flex-col gap-2 hover:scale-105 transition-all"
+>
+	<img {src} alt={title} class="w-full aspect-[4/1]" />
+	<div class="flex flex-col md:flex-row justify-between items-start md:items-end gap-1">
+		<h2 class="text-2xl lg:text-4xl">{title}</h2>
+		<h2 class="text-base sm:text-lg lg:text-2xl">{date}</h2>
+	</div>
+</a>
