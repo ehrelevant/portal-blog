21700f0780ecb86bb26446502f36cca0fbf7a25e
Ehren Castillo <ehrencastillo@gmail.com>
2024-09-04 21:55:44 +0800

    feat(NavBar): add basic NavBar

---
+++ src/routes/NavBar.svelte
@@ -0,0 +1,11 @@
+<script>
+	import NavBtn from './NavBtn.svelte';
+</script>
+
+<nav class="sticky top-0 w-full bg-black text-blue">
+	<ul class="flex flex-row ml-16">
+		<li><NavBtn href="/">Home</NavBtn></li>
+		<li><NavBtn href="/blog">Blog</NavBtn></li>
+		<li><NavBtn href="/about">About</NavBtn></li>
+	</ul>
+</nav>
---
+++ src/routes/NavBtn.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let href: string;
+</script>
+
+<a
+	{href}
+	class="block px-6 py-4 text-center text-lg hover:underline {href == $page.url.pathname
+		? 'underline'
+		: ''}"><slot /></a
+>
