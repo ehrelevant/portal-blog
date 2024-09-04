13d72aa234f27e6c8cfff6f34d8edcd7b33115e2
Ehren Castillo <ehrencastillo@gmail.com>
2024-09-04 23:54:55 +0800

    feat: add SVGs as svelte files

---
+++ src/lib/icons/Pointer.svelte
@@ -0,0 +1,9 @@
+<svg width="68" height="68" viewBox="0 0 68 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M2 32.6667L65.3333 2.66669L35.3333 66L28.6667 39.3334L2 32.6667Z"
+		stroke="#262626"
+		stroke-width="3.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
