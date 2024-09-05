93bab78faa46ec0cea1c7b871fa9c388dbe19ace
Ehren Castillo <ehrencastillo@gmail.com>
2024-09-05 21:28:48 +0800

    feat: add team page

---
+++ src/routes/team/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import sample from '$lib/images/sample.png';
+	import Edzo from '$lib/images/team/Edzo.png';
+	import Ehren from '$lib/images/team/Ehren.jpg';
+	import Yuwen from '$lib/images/team/Yuwen.jpg';
+	import TeamCard from './TeamCard.svelte';
+</script>
+
+<section class="grow h-full flex flex-col sm:flex-row sm:justify-center gap-12 sm:gap-4 px-6">
+	<div class="relative hidden lg:flex justify-center min-h-full w-16 shrink-0">
+		<div class="min-h-full border-l-4 border-black" />
+	</div>
+	<div
+		class="my-8 w-full min-h-full grow grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-x-6 gap-y-4"
+	>
+		<TeamCard src={Edzo} name="Edzo Acyatan" />
+		<TeamCard src={sample} name="Lawrence Bermudez" />
+		<TeamCard src={Ehren} name="Ehren Castillo" />
+		<TeamCard src={Yuwen} name="Yuwen Saavedra" />
+	</div>
+</section>
---
+++ src/routes/team/TeamCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+
+	export let src: string;
+	export let name: string;
+</script>
+
+<div
+	class="bg-white sm:h-80 lg:h-72 w-full rounded-xl p-2 md:p-4 flex flex-col sm:flex-row gap-2 hover:scale-[1.025] transition-transform"
+>
+	<img {src} alt={name} class="w-full sm:w-auto min-h-full aspect-square" />
+	<div class="flex flex-col md:flex-row gap-1">
+		<h2 class="text-4xl sm:text-5xl lg:text-4xl">{name}</h2>
+	</div>
+</div>
