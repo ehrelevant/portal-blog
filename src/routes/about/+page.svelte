3c4f70bbcaf674a513a9a7fa8272e1a9c06e5b3f
Ehren Castillo <ehrencastillo@gmail.com>
2024-09-05 21:42:35 +0800

    chore: run formatter and linter

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { base } from '$app/paths';
 	import src from '$lib/images/sample.png';
 </script>
 
--- src/routes/team/TeamCard.svelte
+++ src/routes/team/TeamCard.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { base } from '$app/paths';
-
 	export let src: string;
 	export let name: string;
 </script>
