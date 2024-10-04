ca77126373c190dd889027d0f9b51bfc73e38bbd
Ehren Castillo <ehrencastillo@gmail.com>
2024-10-05 01:59:04 +0800

    deps: install adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/portal-blog'
-		}
+		adapter: adapter({})
 	}
 };
 
