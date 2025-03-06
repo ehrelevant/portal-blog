a2ced96782a01e6debb9be49b64d1f35b49a6f3f
Ehren Castillo <ehrencastillo@gmail.com>
2025-03-07 00:08:20 +0800

    feat: add software testing report page for GVEN

---
+++ src/routes/blog/15/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import BlogTemplate from '$lib/components/BlogTemplate.svelte';
+	import sample from '$lib/images/sample.webp';
+</script>
+
+<BlogTemplate src={sample} title="Software Testing Report (GVEN)" date="03/06/2025">
+	<div class="w-full max-w-full flex justify-center">
+		<iframe
+			title="Software Testing Report (GVEN)"
+			src="https://drive.google.com/file/d/18gUcGZJS83C__WePvFNKIX_qZhz-qNj4/preview"
+			width="1800"
+			height="640"
+			allow="autoplay"
+			class="max-w-full"
+		></iframe>
+	</div>
+</BlogTemplate>
