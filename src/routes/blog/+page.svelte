f17b77ea0abeec86e2bc458e878e03c7f49a370d
Ehren Castillo <ehrencastillo@gmail.com>
2025-03-11 02:29:02 +0800

    feat: add weekly blog 06 page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -9,6 +9,7 @@
 		<div class="min-h-full border-l-4 border-black" />
 	</div>
 	<div class="my-8 grid grid-flow-row auto-rows-min grid-cols-1 lg:grid-cols-2 gap-x-6 gap-y-4">
+		<BlogCard href="/blog/16" src={sample} title="Weekly Blog 06" date="03/11/2025" />
 		<BlogCard
 			href="/blog/15"
 			src={sample}
---
+++ src/routes/blog/16/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import BlogTemplate from '$lib/components/BlogTemplate.svelte';
+	import sample from '$lib/images/sample.webp';
+</script>
+
+<BlogTemplate src={sample} title="Weekly Blog 06" date="03/11/2025">
+	<div class="w-full max-w-full flex justify-center">
+		<iframe
+			title="Weekly Blog 06"
+			src="https://drive.google.com/file/d/17EEdXT2tQwtHXuCO9Uouqe9gC6Wfw5SH/preview"
+			width="1800"
+			height="640"
+			allow="autoplay"
+			class="max-w-full"
+		></iframe>
+	</div>
+</BlogTemplate>
