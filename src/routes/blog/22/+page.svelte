09d36633047df03e68ca2dc2d43ce0c2f9e5ac9b
Ehren Castillo <ehrencastillo@gmail.com>
2025-04-14 23:20:06 +0800

    feat: add weekly blog 11 page

---
+++ src/routes/blog/22/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import BlogTemplate from '$lib/components/BlogTemplate.svelte';
+	import sample from '$lib/images/sample.webp';
+</script>
+
+<BlogTemplate src={sample} title="Weekly Blog 11" date="04/15/2025">
+	<div class="w-full max-w-full flex justify-center">
+		<iframe
+			title="Weekly Blog 11"
+			src="https://drive.google.com/file/d/15WoSYffsClNLLIAtnHGuZQSUKLhvns1m/preview"
+			width="1800"
+			height="640"
+			allow="autoplay"
+			class="max-w-full"
+		></iframe>
+	</div>
+</BlogTemplate>
