42d207e160bf28cb492003aba6e01ecfa756d050
Ehren Castillo <ehrencastillo@gmail.com>
2025-05-24 20:25:54 +0800

    feat: add project evaluation 2 page

---
+++ src/routes/blog/30/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import BlogTemplate from '$lib/components/BlogTemplate.svelte';
+	import sample from '$lib/images/sample.webp';
+</script>
+
+<BlogTemplate src={sample} title="Project Evaluation 2" date="02/21/2025">
+	<div class="w-full max-w-full flex justify-center">
+		<iframe
+			title="Project Evaluation 2"
+			src="https://drive.google.com/file/d/1AGaMFFN_EdNaT4vp2Km1AAC42GiqAPMT/preview"
+			width="1800"
+			height="640"
+			allow="autoplay"
+			class="max-w-full"
+		></iframe>
+	</div>
+</BlogTemplate>
